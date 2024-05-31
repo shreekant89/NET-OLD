af6adf1ec65d1e08d69298e5c138aa3cf3e24df3
Shreekant Wagh <shreekant.wagh@scania.com>
2024-05-31 16:21:41 +0200

    add old project

--- DockerApi/obj/Debug/net8.0/DockerApi.AssemblyInfo.cs
+++ DockerApi/obj/Debug/net8.0/DockerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba5171b14a363b86635224297a33f0c3e72bb6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ distinct.orderby/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = Enumerable.Range(1, 1000).ToList();
+
+        // Add 100 duplicate numbers (for example, duplicates of the first 100 numbers)
+        numbers.AddRange(Enumerable.Range(1, 100));
+
+        // Shuffle the list to mix the numbers (optional, but makes it more realistic)
+        var rng = new Random();
+        numbers = numbers.OrderBy(x => rng.Next()).ToList();
+
+        //var numbers = new List<int> { 5, 1, 3, 3, 4, 6, 7, 8, 9, 10, 10, 6,20,19,18,17,17,15,16,12,11,13,14,12,21,98,22,97 };
+
+        // Measure time for Distinct().OrderBy()
+        var stopwatch = Stopwatch.StartNew();
+        var distinctThenOrderBy = numbers.Distinct().OrderBy(x => x).ToList();
+        stopwatch.Stop();
+        var distinctThenOrderByTime = stopwatch.ElapsedTicks;
+
+        // Measure time for OrderBy().Distinct()
+        stopwatch.Restart();
+        var orderByThenDistinct = numbers.OrderBy(x => x).Distinct().ToList();
+        stopwatch.Stop();
+        var orderByThenDistinctTime = stopwatch.ElapsedTicks;
+
+        // Output results
+        Console.WriteLine("Distinct().OrderBy(): ");
+        //distinctThenOrderBy.ForEach(n => Console.Write(n + " "));
+        Console.WriteLine($"\nExecution time (ticks): {distinctThenOrderByTime}");
+
+        Console.WriteLine("\nOrderBy().Distinct(): ");
+        //orderByThenDistinct.ForEach(n => Console.Write(n + " "));
+        Console.WriteLine($"\nExecution time (ticks): {orderByThenDistinctTime}");
+        Console.ReadLine();
+    }
+}
